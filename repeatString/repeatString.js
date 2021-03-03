efd34baea3e766901bad6d58c38f08ba012fd248
sb2356-iiitr <soumyabrata.btech.cs19@iiitranchi.ac.in>
2021-03-03 20:20:16 +0400

    completed repeating_strings

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+	let s = "";
+	if (num < 0)
+		s = "ERROR";
+	else {
+	for (let i = 1; i <= num; i++)
+	{
+		s += str;
+	}
+	}
+	return s;
 }
 
 module.exports = repeatString
