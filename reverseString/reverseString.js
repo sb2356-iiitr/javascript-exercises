48c7458a81ff96f3b5406999cbe926f49501a53c
sb2356-iiitr <soumyabrata.btech.cs19@iiitranchi.ac.in>
2021-03-03 22:16:48 +0400

    completed reverse_string

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let s = str.split("");
+    let ss = s.reverse();
+    return ss.join("");
 }
 
 module.exports = reverseString
