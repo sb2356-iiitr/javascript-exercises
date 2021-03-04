76855f8458b7d2f791920de9be0bc7b109b6c834
sb2356-iiitr <soumyabrata.btech.cs19@iiitranchi.ac.in>
2021-03-04 18:29:07 +0400

    completes remove from array

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...a) {
+    const ar = a[0];
+    for (let i = 1; i < a.length; i++) {
+        if (typeof(a[i] == "number")) {
+            for (let j = 0; j < ar.length; j++) {
+                if(ar[j] === a[i])
+                {
+                    ar.splice(j, 1);
+                }
+            }
+        }
+    }
+    return ar;
 }
 
 module.exports = removeFromArray
