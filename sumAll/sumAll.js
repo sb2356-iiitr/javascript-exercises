6ddc5456015b4a342d3bff1906058940466d8ec6
sb2356-iiitr <soumyabrata.btech.cs19@iiitranchi.ac.in>
2021-03-04 18:38:56 +0400

    completed sum_array

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (typeof(a) != "number" || typeof(b) != "number"
+        || a < 0 || b < 0) {
+        return "ERROR";
+    } 
+    let m = Math.max(a, b), mi = Math.min(a, b);
+    let s = m*(m+1)/2 - (mi-1);
+    return s;
 }
 
 module.exports = sumAll
